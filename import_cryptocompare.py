daf8c405c5838d06cda412a619ea22d37e26cd83
chrismslist <cgrab713@gmail.com>
2021-11-03 18:28:04 -0400

    improved comments

--- import_cryptocompare.py
+++ import_cryptocompare.py
@@ -1,10 +1,10 @@
-import os
-import cryptocompare #Import Crypto Database API
-from pushover import init, Client #Import Mobile Push Service API
-import schedule
-import time
-from datetime import datetime
-from colored import fg, bg, attr
+import os #Import OS Package
+import cryptocompare #Import Crypto Database API to get Crypto Prices
+from pushover import init, Client #Import Mobile Push Service API 
+import schedule #Import Scheduling Service for Repeating Functions
+import time #Import Time Package to get Current Time
+from datetime import datetime #Import DateTime Package to get Date and Time
+from colored import fg, bg, attr #Import Colored Package to Color Text
 
 #Define Colors for Colored Text
 color = bg('blue') + fg('white')
@@ -16,6 +16,7 @@ reset = attr('reset')
 clientChristian = Client("u7qr7w4m9x3jqxg1o1f9829seqokbo", api_token="as15vtro6wm8abbcsm9kf85kxphvwc") 
 clientKyle = Client("undjhx9dacw4473ap9oovtka4zvykx", api_token="ahyqq71me344th3j8rvg2vi2nee84o")
 
+#Function w Parameters to Easily Send a Phone Notification
 def sendNotification(message, header):
    clientChristian.send_message(str(message), title=str(header))
    clientKyle.send_message(str(message), title=str(header))
